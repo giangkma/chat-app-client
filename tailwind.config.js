7e0cb099e2f917431b2f7a3cfe14df6e5b6acd70
Đặng Trường Giang <57037541+giangkma@users.noreply.github.com>
2022-02-15 22:47:20 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {
+        extend: {s
             colors: {
                 brightGreen: '#4fb802',
                 oliveGreen: '#468302',
